707b46d04248bf351aee57091e5cb464775444ed
lstatro <jdhoffmeyer@gmail.com>
2022-11-24 18:17:21 +0000

    abstract control handler

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,120 @@
+import {
+  AWSProfileName,
+  AWSRegionName,
+  CommonInventoryItem,
+  ControlProcedure,
+  CredentialedClassArgs,
+  FinalControlValidationResult,
+  ResourceName,
+} from '@elemental-clouds/hydrogen';
+
+import { CommandBuilder } from 'yargs';
+import { Common } from '@elemental-clouds/osmium/lib/Common';
+import { S3 as Service } from '@elemental-clouds/oxygen';
+import assert from 'assert';
+import titanium from '@elemental-clouds/titanium';
+
 export const commandDirOptions = {
   /** ts needed for ts-node dev time testing, js for production */
   extensions: ['js', 'ts'],
 };
+
+export interface IronArgs {
+  regions: AWSRegionName[];
+  profile?: AWSProfileName;
+  resourceIds?: ResourceName[];
+}
+
+interface IronHandlerInterface extends IronArgs {
+  Scanner: new (args: CredentialedClassArgs) => Common;
+}
+
+export class IronHandler {
+  args: IronHandlerInterface;
+  regions?: AWSRegionName[];
+  profile?: AWSProfileName;
+  resourceIds?: ResourceName[];
+  results: FinalControlValidationResult[] = [];
+
+  static getService(service: object) {
+    return service as { [key: string]: ControlProcedure };
+  }
+
+  static getServiceControls(service: object) {
+    return Object.keys(service);
+  }
+
+  static setupBuilder() {
+    const service = IronHandler.getService(Service);
+    const controls = IronHandler.getServiceControls(Service);
+
+    const builder: CommandBuilder = {};
+
+    for (const control of controls) {
+      const _name = service[control]?.name;
+      const description = service[control]?.description;
+
+      assert(service[control], `unknown control ${control}`);
+      assert(_name, 'missing control name');
+      assert(description, 'missing control description');
+
+      builder[_name] = {
+        description,
+      };
+    }
+    return builder;
+  }
+
+  constructor(args: IronHandlerInterface) {
+    this.args = args;
+    this.regions = args.regions;
+    this.profile = args.profile;
+    this.resourceIds = args.resourceIds;
+  }
+
+  getFirstRegionInArgs(regions: AWSRegionName[] | undefined) {
+    assert(Array.isArray(regions));
+    assert(regions.length === 1);
+
+    const region = regions[0];
+
+    assert(region, 'unable to determine region');
+    return region;
+  }
+
+  async getInventory() {
+    const { regions, profile, resourceIds, Scanner } = this.args;
+    const region = this.getFirstRegionInArgs(regions);
+
+    const scanner = new Scanner({ region, profile });
+
+    if (resourceIds) {
+      scanner.addResource({ resources: resourceIds });
+    }
+
+    await scanner.enumerate();
+
+    return scanner.inventory as unknown as CommonInventoryItem[];
+  }
+
+  async getControlResults() {
+    const controls = IronHandler.getServiceControls(Service);
+    const service = IronHandler.getService(Service);
+
+    for (const control of controls) {
+      if (control in this.args) {
+        for (const item of await this.getInventory()) {
+          const procedure = service[control]?.procedure;
+          assert(procedure, `unknown control ${control} procedure`);
+          this.results.push(
+            titanium(item, procedure) as unknown as FinalControlValidationResult
+          );
+        }
+      }
+    }
+
+    for (const result of this.results) {
+      console.log(JSON.stringify(result, null, 2));
+    }
+  }
+}
--- src/s3/buckets/controls.ts
+++ src/s3/buckets/controls.ts
@@ -1,88 +1,18 @@
 import * as osmium from '@elemental-clouds/osmium';
 
-import { Arguments, CommandBuilder } from 'yargs';
+import { IronArgs, IronHandler } from '../../common';
 
-import { ControlProcedure } from '@elemental-clouds/hydrogen';
-import { S3 as Service } from '@elemental-clouds/oxygen';
-import assert from 'assert';
-import titanium from '@elemental-clouds/titanium';
+import { Arguments } from 'yargs';
 
 export const command = 'control';
 export const describe = 'S3 bucket public access control toggles';
+export const builder = IronHandler.setupBuilder();
 
-function getService(service: object) {
-  return service as { [key: string]: ControlProcedure };
-}
+export const handler = async function (args: Arguments<IronArgs>) {
+  const iron = new IronHandler({
+    ...args,
+    Scanner: osmium.S3.Buckets,
+  });
 
-function getServiceControls(service: object) {
-  return Object.keys(service);
-}
-
-function setupBuilder() {
-  const service = getService(Service);
-  const controls = getServiceControls(Service);
-
-  const builder: CommandBuilder = {};
-
-  for (const control of controls) {
-    const _name = service[control]?.name;
-    const description = service[control]?.description;
-
-    assert(service[control], `unknown control ${control}`);
-    assert(_name, 'missing control name');
-    assert(description, 'missing control description');
-
-    builder[_name] = {
-      description,
-    };
-  }
-  return builder;
-}
-
-function getFirstRegionInArgs(regions: string[] | undefined) {
-  assert(Array.isArray(regions));
-  assert(regions.length === 1);
-
-  const region = regions[0];
-
-  assert(region, 'unable to determine region');
-  return region;
-}
-
-export const builder = setupBuilder();
-
-export const handler = async function (
-  args: Arguments<{
-    regions: string[];
-    profile?: string;
-    resourceIds?: string[];
-  }>
-) {
-  const { regions, profile, resourceIds } = args;
-  const controls = getServiceControls(Service);
-  const region = getFirstRegionInArgs(regions);
-  const service = getService(Service);
-  const results = [];
-
-  const scanner = new osmium.S3.Buckets({ profile, region });
-
-  if (resourceIds) {
-    scanner.addResource({ resources: resourceIds });
-  }
-
-  await scanner.enumerate();
-
-  for (const control of controls) {
-    if (control in args) {
-      for (const item of scanner.inventory) {
-        const procedure = service[control]?.procedure;
-        assert(procedure, `unknown control ${control} procedure`);
-        results.push(titanium(item, procedure));
-      }
-    }
-  }
-
-  for (const result of results) {
-    console.log(JSON.stringify(result, null, 2));
-  }
+  await iron.getControlResults();
 };
